7c514edf63907e792bbafc2dcdf552a0c5c4fce0
twstddev <twstd.dev@gmail.com>
2014-05-14 20:43:53 +0000

    Add menu items migration

---
+++ app/database/migrations/2014_05_14_203928_create_menu_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenuItemsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('menu_items', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string( 'title' );
+			$table->string( 'url' );
+			$table->integer( 'parent_id' )->unsigned();
+			$table->integer( 'position' );
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('menu_items');
+	}
+
+}
