a962dce66f27299735ae97b04a4682a7d31e687b
twstddev <twstd.dev@gmail.com>
2014-05-15 21:58:20 +0000

    Add sluggable

--- app/models/Page.php
+++ app/models/Page.php
@@ -1,13 +1,33 @@
 <?php 
+use \LaravelBook\Ardent\Ardent;
+
 /**
  * @brief Implements a Page object.
  */
-class Page extends Eloquent {
+class Page extends Ardent {
+	/**
+	 * @brief Constructor that adds custom validation
+	 * to make sure that given template is allowed.
+	 */
+	public function __construct( $attributes = array() ) {
+		parent::__construct( $attributes );
+	}
+
+	public static $sluggable = array(
+		'build_from' => 'title',
+		'save_to' => 'slug'
+	);
+
+	public static $rules = array(
+		'title' => 'required',
+		'slug' => 'required|unique:pages'
+	);
+
 	protected $fillable = array(
 		'title',
 		'slug',
 		'template',
 		'body',
-		'properties'
+		'meta'
 	);
 }
