2a037e895a863a09ba5fb93b73be99f94c2e6231
twstddev <twstd.dev@gmail.com>
2014-05-20 21:22:21 +0000

    Fix declaration issue

--- app/helpers.php
+++ app/helpers.php
@@ -8,7 +8,9 @@ namespace Admin;
  * @param[in] String $controll_name is the name to check
  * against
  */
-function is_active_controller( $controller_name ) {
-	//return ( \Request::is( 'admin/' . $controller_name  ) ) ? 'active' : '';
-	return '';
+if ( !function_exists( 'Admin\is_active_controller' ) ) {
+	function is_active_controller( $controller_name ) {
+		//return ( \Request::is( 'admin/' . $controller_name  ) ) ? 'active' : '';
+		return '';
+	}
 }
