a7c901767118c68f37bbe9ecf0bc9dc02effa7fb
twstddev <twstd.dev@gmail.com>
2014-05-21 20:04:15 +0000

    Fix meta fields loading error

--- app/views/admin/pages/templates/home.blade.php
+++ app/views/admin/pages/templates/home.blade.php
@@ -6,13 +6,20 @@ $slide_form = View::make( 'admin.pages.templates.form.slide', array(
 		'summary' => ''
    	)
 ) );
+
+$last_index = isset( $page->meta[ 'slider' ] ) ? count( $page->meta[ 'slider' ] ) : 0;
+
+$current_index = 0;
 ?>
-<div class="nested-meta-fields" data-index="{{ count( $page->meta[ 'slider' ] ) }}">
-	@foreach ( $page->meta[ 'slider' ] as $index => $slide )
-		@include( 'admin.pages.templates.form.slide', array( 'namespace' => 'meta[slider][' . ( $index + 1 ) . ']', 'values' => $slide ) )
-	@endforeach
+<div class="nested-meta-fields" data-index="{{ $last_index }}">
+	@if ( $last_index )
+		@foreach ( $page->meta[ 'slider' ] as $index => $slide )
+			@include( 'admin.pages.templates.form.slide', array( 'namespace' => 'meta[slider][' . $current_index . ']', 'values' => $slide ) )
+			<?php $current_index++; ?>
+		@endforeach
+	@endif
 </div>
-<a href="#" class="btn btn-primary has-many-meta" data-placeholder="NEW_SLIDER" data-html='{{{ $slide_form }}}'>
+<a href="#" class="btn btn-primary has-many-meta" data-placeholder="NEW_SLIDER" data-html="{{{ $slide_form }}}">
 	Add slide
 </a>
 
